fe0e53a07e5cfcfc654e84f3c118cacd5a6cce8e
HuynhAnhNguyen <119774462+HuynhAnhNguyen@users.noreply.github.com>
2023-06-29 16:00:06 +0700

    Update congviec.routes.js

--- backend/app/routes/congviec.routes.js
+++ backend/app/routes/congviec.routes.js
@@ -1,55 +1,59 @@
 const express = require("express");
-const router = express.Router();
+const congViecRouter = express.Router();
 const congViecController = require("../controllers/congviec.controller");
 const { checkRole } = require('../helpers/checkRole.js');
 const { verifyToken } = require('../helpers/verifyToken.js');
 
-congViecController.use(verifyToken);
 
+// congViecController.use(verifyToken);
+congViecRouter.use(verifyToken);
 // Retrieve all congviec
-router.get("/congviec", congViecController.findAll);
+congViecRouter.get("/congviec", congViecController.findAll);
 
 // Retrieve 1 congviec; id là mã công việc
-router.get("/congviec/:id", congViecController.findid);
+congViecRouter.get("/congviec/:id", congViecController.findid);
 
 // Create a new congviec; id là mã dự án
-router.post("/congviec/:id", congViecController.create);
+congViecRouter.post("/congviec/:id", checkRole(['director', 'manager']), congViecController.create);
 
 // id là mã dự án
-router.post("/congviecfile/:id", congViecController.createFromFile);
+congViecRouter.post("/congviecfile/:id", checkRole(['director', 'manager']), congViecController.createFromFile);
 
 // Retrieve a single congviec with id
-router.get("/timCongViec", congViecController.findOne);
+congViecRouter.get("/timCongViec", congViecController.findOne);
 
 // id là mã dự án
-router.get("/congviecbyduan/:id", congViecController.findByDuAn);
+congViecRouter.get("/congviecbyduan/:id", congViecController.findByDuAn);
 
 // Update a congviec with id
 // id là mã công việc
-router.put("/congviec/:id", congViecController.update);
+congViecRouter.put("/congviec/:id", checkRole(['director', 'manager']), congViecController.update);
 
 // Delete a congviec with id
 // id là mã công việc
-router.delete("/congviec/:id", congViecController.delete);
+congViecRouter.delete("/congviec/:id", checkRole(['director', 'manager']), congViecController.deleteTT);
 
 // Delete all congviec
 // id là mã công việc
-router.delete("/xoaCongViecVinhVien/:id", congViecController.deleteVV);
+congViecRouter.delete("/xoaCongViecVinhVien/:id", checkRole(['director', 'manager']), congViecController.deleteVV);
 
 // Phân công công việc và tải lên tệp tin
 // id là mã công việc
-router.post("/congviec/phanCongCongViec/:id", congViecController.phanCongCongViec);
+congViecRouter.post("/congviec/phanCongCongViec/:id", checkRole(['director', 'manager']), congViecController.phanCongCongViec);
 
 // Sửa phân công công việc và tải lên tệp tin
 // id là mã công việc
-router.put("/congviec/editPhanCongCongViec/:id", congViecController.editPhanCongCongViec);
+congViecRouter.put("/congviec/editPhanCongCongViec/:id", checkRole(['director', 'manager']), congViecController.editPhanCongCongViec);
 
 
 // Retrieve 1 congviec; id là mã công việc
-router.get("/nguoidungbycongviec/:id", congViecController.findNguoiDungByCongViec);
+congViecRouter.get("/nguoidungbycongviec/:id", congViecController.findNguoiDungByCongViec);
 
 
 // Retrieve 1 congviec; id là mã công việc
-router.get("/tailieubycongviec/:id", congViecController.findTaiLieuByCongViec);
+congViecRouter.get("/tailieubycongviec/:id", congViecController.findTaiLieuByCongViec);
 
-module.exports = router;
+module.exports = congViecRouter;
+
+
+
