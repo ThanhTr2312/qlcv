8096297aa72579226735a2dd26bd279ed3ec5306
ThanhTr2312 <90843354+ThanhTr2312@users.noreply.github.com>
2023-07-07 13:51:04 +0700

    Update thongke.route.js

--- backend/app/routes/thongke.route.js
+++ backend/app/routes/thongke.route.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const thongkeRouter = express.Router();
 const thongkeController = require("../controllers/thongke.controller");
+const { checkRole } = require('../helpers/checkRole.js');
+const { verifyToken } = require('../helpers/verifyToken.js');
 
 // Thống kê
-thongkeRouter.get('/congviec/thongke', thongkeController.getTKCongViecTrangThai);
-thongkeRouter.get('/congviec/uutien/thongke', thongkeController.getTKCongViecUuTien);
-thongkeRouter.get('/nguoidung_congviec/thongke', thongkeController.getTKNDCongViecUuTien);
-thongkeRouter.get('/nguoidung_congviec/trongthang/thongke', thongkeController.getTKNDCongViecTrongThang);
+thongkeRouter.use(verifyToken);
 
-thongkeRouter.get('/nguoidung/thongke', thongkeController.getTKNguoiDung);
+thongkeRouter.get('/congviec/thongke',  checkRole(['director', 'manager']), thongkeController.getTKCongViecTrangThai);
+thongkeRouter.get('/congviec/uutien/thongke',  checkRole(['director', 'manager']), thongkeController.getTKCongViecUuTien);
+thongkeRouter.get('/nguoidung_congviec/thongke',  checkRole(['director', 'manager']), thongkeController.getTKNDCongViecUuTien);
+thongkeRouter.get('/nguoidung_congviec/trongthang/thongke',  checkRole(['director', 'manager']), thongkeController.getTKNDCongViecTrongThang);
 
-thongkeRouter.get('/duan/thongke', thongkeController.getTKDuAn);
+thongkeRouter.get('/nguoidung/thongke',  checkRole(['director', 'manager']), thongkeController.getTKNguoiDung);
+
+thongkeRouter.get('/duan/thongke',  checkRole(['director', 'manager']), thongkeController.getTKDuAn);
 
 
 module.exports = thongkeRouter;
