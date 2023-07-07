15a7f4fcec15706418071d305578bdf2ea19564f
ThanhTr2312 <90843354+ThanhTr2312@users.noreply.github.com>
2023-07-07 13:49:00 +0700

    Update duan.routes.js

--- backend/app/routes/duan.routes.js
+++ backend/app/routes/duan.routes.js
@@ -6,13 +6,13 @@ const { verifyToken } = require('../helpers/verifyToken.js');
 
 duanRouter.use(verifyToken);
 //Thêm dự án
-duanRouter.post('/duan', checkRole('director'), duanController.postDuAn);
+duanRouter.post('/duan',  checkRole(['director', 'manager']), duanController.postDuAn);
 //Xóa dự án
-duanRouter.delete('/duan/:id', checkRole('director'), duanController.deleteDuAn);
+duanRouter.delete('/duan/:id',  checkRole(['director', 'manager']), duanController.deleteDuAn);
 //lấy hết dự án
 duanRouter.get('/duan', checkRole(['director', 'manager']), duanController.getAllDuAn);
 //sửa dự án
-duanRouter.put('/duan/:id', checkRole('director'), duanController.putDuAn);
+duanRouter.put('/duan/:id', c checkRole(['director', 'manager']), duanController.putDuAn);
 //Tìm dự án theo id, phương thức GET
 duanRouter.get('/duan/:id', checkRole(['director', 'manager']), duanController.getDuAn);
 
